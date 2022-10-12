4c532f959bdd7c0d0095aba9d1a4e4b625108c24
Simon <shelmig@two.inc>
2022-10-12 05:17:43 +0000

    Adding dummy training pipeline for CML setup

---
+++ src/pipelines/train_pipeline.py
@@ -0,0 +1,39 @@
+from src.util import path 
+
+
+import pandas as pd
+from sklearn.dummy import DummyClassifier
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+RANDOM_SEED = 0
+
+def train_pipeline():
+    """Dummy Pipeline to test CML"""
+    data = pd.read_csv(path.TRAIN_DATA_PATH)
+    numerical_columns = ['step','amount']
+    target_column = 'fraud'
+    X = data.loc[:,numerical_columns]
+    y = data.loc[:, target_column]
+    X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=RANDOM_SEED)
+    clf = DummyClassifier()
+    model = clf.fit(X_train,y_train)
+    y_pred = clf.predict(X_test)
+    metrics = {
+        'f1_score': f1_score(y_test, y_pred),
+        'roc_auc': roc_auc_score(y_test,y_pred),
+        'accuracy': accuracy_score(y_test,y_pred)
+    }
+    with open(path.METRICS_PATH, 'w') as f:
+        for key, val in metrics.items():
+            f.write(f'{key} - {val}\n')
+    
+
+    
+
+
+if __name__ == '__main__':
+    train_pipeline()
+    
