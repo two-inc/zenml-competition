7afaa17aa2863768c05ece152a22f517e8a8874c
vangelis <vangelis@two.inc>
2022-10-18 20:54:17 +0000

    Docstrings squared

--- src/steps/evaluator.py
+++ src/steps/evaluator.py
@@ -1,3 +1,4 @@
+"""Evaluator step"""
 import pandas as pd
 from typing import Dict
 from sklearn.metrics import r2_score
--- src/steps/trainer.py
+++ src/steps/trainer.py
@@ -1,3 +1,4 @@
+"""Trainer step"""
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.base import ClassifierMixin
