a59bd634f01e0c5e46e95886ec2ec5303145c6f6
vangelis <vangelis@two.inc>
2022-10-19 07:11:02 +0000

    Removes pdb

--- src/steps/importer.py
+++ src/steps/importer.py
@@ -3,7 +3,6 @@ import pandas as pd
 from zenml.steps import step, Output
 from zenml.logger import get_logger
 from src.util.data_access import load_data
-import pdb
 
 logger = get_logger(__name__)
 
@@ -12,5 +11,4 @@ logger = get_logger(__name__)
 def importer() -> Output(data=pd.DataFrame):
     """Loads the raw fraud dataset fro GCP."""
     data = load_data()
-    pdb.set_trace()
     return data
