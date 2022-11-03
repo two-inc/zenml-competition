6e4d72504f37ec19097708e9743935adb702aa0f
Simon <shelmig@two.inc>
2022-11-03 07:35:25 +0000

    shelmig/chore: Further integrating the streamlit app with our Seldon model

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -1,65 +1,43 @@
 """streamlit app"""
-
-from typing import cast
 import json
+from functools import lru_cache
+from typing import cast
+from typing import Optional
+
 import streamlit as st
 from zenml.integrations.seldon.model_deployers import SeldonModelDeployer
 from zenml.integrations.seldon.services import SeldonDeploymentService
 from zenml.logger import get_logger
 
+from src.util import columns
 from src.util.data_access import load_data
+from src.util.preprocess import get_preprocessed_data
 
 logger = get_logger(__name__)
 
 
 def main():
     """Main function for streamlit"""
-    _ = load_data()
+    data = get_data()
+    mean_fraud_rate = get_mean_value(data.fraud)
+    service = get_service()
 
     with st.sidebar:
-        step = st.number_input("Step", step=1, min_value=1, max_value=365)
-        amount = st.slider("amount")
-        mean_category_amount_previous_step = st.slider(
-            "mean_category_amount_previous_step"
+        st.title("Select Model Parameter Values")
+        transaction_number = st.number_input(
+            "Transaction Number",
+            min_value=get_min_value(data.index),
+            max_value=get_max_value(data.index),
         )
-        customer_amount_ma_total = st.slider("customer_amount_ma_total")
-        customer_amount_ma_10 = st.slider("customer_amount_ma_10")
-        customer_amount_ma_5 = st.slider("customer_amount_ma_5")
-        customer_amount_mstd_total = st.slider("customer_amount_mstd_total")
-        customer_amount_mstd_10 = st.slider("customer_amount_mstd_10")
-        customer_amount_mstd_5 = st.slider("customer_amount_mstd_5")
-        merchant_amount_ma_total = st.slider("merchant_amount_ma_total")
-        merchant_amount_ma_50 = st.slider("merchant_amount_ma_50")
-        merchant_amount_ma_10 = st.slider("merchant_amount_ma_10")
-        merchant_amount_ma_5 = st.slider("merchant_amount_ma_5")
-        merchant_amount_ma_3 = st.slider("merchant_amount_ma_3")
-        merchant_amount_mstd_total = st.slider("merchant_amount_mstd_total")
-        merchant_amount_mstd_50 = st.slider("merchant_amount_mstd_50")
-        merchant_amount_mstd_10 = st.slider("merchant_amount_mstd_10")
-        merchant_amount_mstd_5 = st.slider("merchant_amount_mstd_5")
-        merchant_amount_mstd_3 = st.slider("merchant_amount_mstd_3")
-        category_amount_ma_total = st.slider("category_amount_ma_total")
-        category_amount_ma_100 = st.slider("category_amount_ma_100")
-        category_amount_ma_10 = st.slider("category_amount_ma_10")
-        category_amount_mstd_total = st.slider("category_amount_mstd_total")
-        category_amount_mstd_100 = st.slider("category_amount_mstd_100")
-        category_amount_mstd_10 = st.slider("category_amount_mstd_10")
-        merchant_amount_moving_max = st.slider("merchant_amount_moving_max")
-        customer_amount_moving_max = st.slider("customer_amount_moving_max")
-        category_amount_moving_max = st.slider("category_amount_moving_max")
-        customer_fraud_commited_mean = st.slider(
-            "customer_fraud_commited_mean"
-        )
-        merchant_fraud_commited_mean = st.slider(
-            "merchant_fraud_commited_mean"
-        )
-        category_fraud_commited_mean = st.slider(
-            "category_fraud_commited_mean"
-        )
+        if st.checkbox("Replicate Transaction"):
+            transaction = data.loc[transaction_number, :]
+        else:
+            transaction = None
+        inputs = get_inputs(data, transaction)
 
     st.title("Detecting Fraudulent Financial Transactions")
 
-    st.markdown("<Problem statement>")
+    st.markdown("")
 
     st.markdown(
         """
@@ -71,84 +49,98 @@ def main():
     )
 
     if st.button("Predict"):
-        model_deployer = SeldonModelDeployer.get_active_model_deployer()
-
-        services = model_deployer.find_model_server(
-            pipeline_name="continuous_deployment_pipeline_3",
-            pipeline_step_name="seldon_model_deployer_step",
-            model_name="model",
-        )
-        print(services)
-        if services:
-            service = cast(SeldonDeploymentService, services[0])
-            if service.is_running:
-                print(
-                    f"The Seldon prediction server is running remotely as a Kubernetes "
-                    f"service and accepts inference requests at:\n"
-                    f"    {service.prediction_url}\n"
-                    f"To stop the service, run "
-                    f"[italic green]`zenml model-deployer models delete "
-                    f"{str(service.uuid)}`[/italic green]."
-                )
-            elif service.is_failed:
-                print(
-                    f"The Seldon prediction server is in a failed state:\n"
-                    f" Last state: '{service.status.state.value}'\n"
-                    f" Last error: '{service.status.last_error}'"
-                )
-
-        else:
-            print(
-                "No Seldon prediction server is currently running. The deployment "
-                "pipeline must run first to train a model and deploy it. Execute "
-                "the same command with the `--deploy` argument to deploy a model."
-            )
-
-        pred = [
-            [
-                step,
-                amount,
-                mean_category_amount_previous_step,
-                customer_amount_ma_total,
-                customer_amount_ma_10,
-                customer_amount_ma_5,
-                customer_amount_mstd_total,
-                customer_amount_mstd_10,
-                customer_amount_mstd_5,
-                merchant_amount_ma_total,
-                merchant_amount_ma_50,
-                merchant_amount_ma_10,
-                merchant_amount_ma_5,
-                merchant_amount_ma_3,
-                merchant_amount_mstd_total,
-                merchant_amount_mstd_50,
-                merchant_amount_mstd_10,
-                merchant_amount_mstd_5,
-                merchant_amount_mstd_3,
-                category_amount_ma_total,
-                category_amount_ma_100,
-                category_amount_ma_10,
-                category_amount_mstd_total,
-                category_amount_mstd_100,
-                category_amount_mstd_10,
-                merchant_amount_moving_max,
-                customer_amount_moving_max,
-                category_amount_moving_max,
-                customer_fraud_commited_mean,
-                merchant_fraud_commited_mean,
-                category_fraud_commited_mean,
-            ]
-        ]
-
         try:
-            predict_format = json.dumps(pred)
+            predict_format = json.dumps(inputs)
             prediction = service.predict(predict_format)
-            st.success(
-                f"Given the customer's historical data, model says LEGITIMATE {prediction}"
-            )
+            y_pred_proba = prediction["data"]["ndarray"][-1][-1]
+            if y_pred_proba > mean_fraud_rate:
+                st.error(
+                    f"The passed transaction has a {100*y_pred_proba:.2f}% probability of being fraudulent"
+                )
+            else:
+                st.success(
+                    f"The passed transaction has a {100*y_pred_proba:.2f}% probability of being fraudulent"
+                )
         except Exception as e:
             logger.error(e)
             st.error("An unknown error occurred, please try again later")
+
+
+@st.cache
+def get_data():
+    data = load_data()
+    return get_preprocessed_data(data)
+
+
+def get_inputs(data: pd.DataFrame, transaction: Optional[pd.Series]):
+    def get_slider(data: pd.DataFrame, col: str):
+        d = data.loc[:, col]
+        min = get_min_value(d)
+        max = get_max_value(d)
+        if transaction is not None:
+            value = get_mean_value(transaction[col])
+        else:
+            value = get_mean_value(d)
+        t = int if d.dtype == int else float
+        return st.slider(
+            col, min_value=t(min), max_value=t(max), value=t(value)
+        )
+
+    return [[get_slider(data, col) for col in columns.NUMERICAL]]
+
+
+@st.cache
+def get_min_value(data):
+    return data.min()
+
+
+@st.cache
+def get_max_value(data):
+    return data.max()
+
+
+@st.cache
+def get_mean_value(data):
+    return data.mean()
+
+
+@st.cache
+def get_service():
+    model_deployer = SeldonModelDeployer.get_active_model_deployer()
+
+    services = model_deployer.find_model_server(
+        pipeline_name="continuous_deployment_pipeline_3",
+        pipeline_step_name="seldon_model_deployer_step",
+        model_name="model",
+    )
+    if services:
+        service = cast(SeldonDeploymentService, services[0])
+        if service.is_running:
+            print(
+                f"The Seldon prediction server is running remotely as a Kubernetes "
+                f"service and accepts inference requests at:\n"
+                f"    {service.prediction_url}\n"
+                f"To stop the service, run "
+                f"[italic green]`zenml model-deployer models delete "
+                f"{str(service.uuid)}`[/italic green]."
+            )
+        elif service.is_failed:
+            print(
+                f"The Seldon prediction server is in a failed state:\n"
+                f" Last state: '{service.status.state.value}'\n"
+                f" Last error: '{service.status.last_error}'"
+            )
+            raise ValueError
+
+    else:
+        print(
+            "No Seldon prediction server is currently running. The deployment "
+            "pipeline must run first to train a model and deploy it. Execute "
+            "the same command with the `--deploy` argument to deploy a model."
+        )
+        return None
+
+    return service
 
 
 if __name__ == "__main__":
